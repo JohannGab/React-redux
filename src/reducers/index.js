af124af3494cb7afde72aef07f9871c74ffaa167
JohannGab <johann.gabelle@outlook.fr>
2019-06-04 16:30:12 +0200

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import areArticlesBeingFetched from './areArticlesBeingFetched';
+import articles from './articles';
+
+
+
+export default combineReducers({
+    areArticlesBeingFetched,
+    articles,
+});
